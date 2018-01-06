9621eb1845e98cfff0aaac706d438f558513e84e
Adhy <adhywiranatap@gmail.com>
2018-01-07 00:45:42 +0700

    update webpack and ts config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,3 @@
-// `CheckerPlugin` is optional. Use it if you want async error reporting.
-// We need this plugin to detect a `--watch` mode. It may be removed later
-// after https://github.com/webpack/webpack/issues/3460 will be resolved.
 const { CheckerPlugin } = require('awesome-typescript-loader');
 const path = require('path');
 
@@ -15,11 +12,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
-
-  // Source maps support ('inline-source-map' also works)
   devtool: 'source-map',
-
-  // Add the loader for .ts files.
   module: {
     loaders: [
       {
