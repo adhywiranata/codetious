75dc725dbdef995b790193cf16ee18d1dc8801b0
Adhy <adhywiranatap@gmail.com>
2018-01-06 18:44:16 +0700

    add getAllVars selector

--- selectors.js
+++ selectors.js
@@ -2,17 +2,25 @@
 const filterVariables = parsed =>
   parsed.filter(p => p.constructor.name === 'VariableDeclaration');
 
+const getAllVariables = parsed => {
+  return filterVariables(parsed).map(variable => ({
+    name: variable.declarations[0].id.name,
+    value: variable.declarations[0].init ? variable.declarations[0].init.value : undefined,
+    kind: variable.kind,
+  }));
+};
+
 const getVariableByName = (parsed, name) => {
   for(vars of filterVariables(parsed)) {
     if(vars.declarations[0].id.name === name) {
       return true;
     }
   }
-
   return false;
 };
 
 module.exports = {
   filterVariables,
   getVariableByName,
-}
+  getAllVariables,
+};
--- test.js
+++ test.js
@@ -3,8 +3,10 @@ const codetious = require('./lib');
 const sampleCode1 = `
 let num = 5;
 const phi = 3.14;
+var x;
 `;
 
 console.log(codetious.getVariableByName(sampleCode1, 'num'));
 console.log(codetious.getVariableByName(sampleCode1, 'phi'));
 console.log(codetious.getVariableByName(sampleCode1, 'pi'));
+console.log(codetious.getAllVariables(sampleCode1));
